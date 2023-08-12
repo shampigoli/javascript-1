188e9dc35d30a7a0c3dda40f8a49abe7f9f0d670
unknown <alijawad1109@gmail.com>
2023-08-12 06:51:51 -0700

    first

---
+++ Javascript 1/js/main.js
@@ -0,0 +1,76 @@
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+
+// let cartItem = document.querySelector('.cart-items-container');
+
+// document.querySelector('#cart-btn').onclick = () =>{
+//     cartItem.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+
+
+// let searchForm = document.querySelector('.search-form');
+
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
+// }
+
+
+
+// window.onscroll = () =>{
+//     nav.classList.remove('active');
+//     searchForm.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+
+
+
+// // let navbar = document.querySelector('.navbar');
+
+// // document.querySelector('#menu-btn').onclick = () =>{
+// //     navbar.classList.toggle('active');
+
+// // }
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    searchForm.classList.remove('active');
+    // cartItem.classList.remove('active');
+} 
+
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+
+
+window.onscroll = () =>{
+    nav.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
